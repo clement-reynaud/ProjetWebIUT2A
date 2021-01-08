62b31df13872300b7f6dabae04a1ba95376e9b86
Marie-Jridi <74175840+Marie-Jridi@users.noreply.github.com>
2021-01-08 09:00:04 +0100

    petits changements de détails

--- Modèle/ModeleUtilisateur.php
+++ Modèle/ModeleUtilisateur.php
@@ -14,11 +14,6 @@ class ModeleUtilisateur{
         return $this->ugt->verifyPseudo($pseudo);
     }
 
-    public function verifyMdp($mdp)
-    {
-        return $this->ugt->verifyMdp($mdp);
-    }
-
     public function verifyConnexion($pseudo,$mdp){
         return $this->ugt->verifyConnexion($pseudo,$mdp);
     }
@@ -56,19 +51,6 @@ class ModeleUtilisateur{
     public function getUti($pseudo){
         return $this->ugt->getUti($pseudo);
     }
-/*
-    function incCookie(): int
-    {
-        if (isset ($_COOKIE["nbCo"])){
-            $nCo=filter_var($_COOKIE["nbCo"], FILTER_SANITIZE_NUMBER_INT);
-            $nCo=$nCo+1;
-        }
-        else{
-            $nCo=1;
-        }
-        setcookie("nbCo", $nCo, time()+3600);
-        return $nCo;
-    }*/
 
     function nbComCookie()
     {
